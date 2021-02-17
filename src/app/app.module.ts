7cb5f72fcb6f500981398761825f8db9713f1c84
Moutah <mathieu.tappolet@gmail.com>
2021-02-18 00:08:37 +0100

    Added route to game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './views/game/game.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, GameComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
