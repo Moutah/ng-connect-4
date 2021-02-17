f3b5350d7b36ff92c20ce58b7d7871191db6efd8
Moutah <mathieu.tappolet@gmail.com>
2021-02-18 00:59:03 +0100

    Minimal layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { GridComponent } from './components/grid/grid.component';
 import { CellComponent } from './components/cell/cell.component';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
-  declarations: [AppComponent, GameComponent, GridComponent, CellComponent],
+  declarations: [AppComponent, GameComponent, GridComponent, CellComponent, TimerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
