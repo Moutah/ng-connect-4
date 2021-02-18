8f22171d431352dafdede2c88364047e4b3ad0c2
Moutah <mathieu.tappolet@gmail.com>
2021-02-18 01:34:12 +0100

    Rearranged files by feature instead as by type

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameComponent } from './views/game/game.component';
+import { GameComponent } from './game/game.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { GridComponent } from './components/grid/grid.component';
-import { CellComponent } from './components/cell/cell.component';
-import { TimerComponent } from './components/timer/timer.component';
+import { GridComponent } from './grid/grid.component';
+import { CellComponent } from './grid/cell/cell.component';
+import { TimerComponent } from './game/timer/timer.component';
 
 @NgModule({
-  declarations: [AppComponent, GameComponent, GridComponent, CellComponent, TimerComponent],
+  declarations: [
+    AppComponent,
+    GameComponent,
+    GridComponent,
+    CellComponent,
+    TimerComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    NgxsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent],
