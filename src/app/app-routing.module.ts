adf30b1b5b2445a150eb15b71be551be49072812
Moutah <mathieu.tappolet@gmail.com>
2021-02-21 02:32:41 +0100

    Made rules page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { HomeComponent } from './pages/home/home.component';
+import { RulesComponent } from './pages/rules/rules.component';
 
 const routes: Routes = [
+  { path: 'rules', component: RulesComponent },
   { path: 'game', component: GameComponent },
   { path: '**', component: HomeComponent },
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { RulesComponent } from './rules/rules.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, RulesComponent],
   imports: [CommonModule, MaterialModule, RouterModule],
 })
 export class PagesModule {}
